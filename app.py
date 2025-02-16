2975d01d249504d48a0980dca112a053b78ef1ef
myckey <mungamykey98@gmail.com>
2025-02-16 11:59:15 +0300

    changes

--- app.py
+++ app.py
@@ -227,4 +227,4 @@ if __name__ == '__main__':
     socketio.run(app, debug=True, port=5000)
 
     if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+        socketio.run(app, debug=True, host='0.0.0.0', port=5000)
